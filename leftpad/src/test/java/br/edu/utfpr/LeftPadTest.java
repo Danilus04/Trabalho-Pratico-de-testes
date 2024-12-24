eb38a6b3632cf2c0c887a7f88b4e124a1c66a4dc
Danilus04 <danilogarciaba@hotmail.com>
2024-12-24 13:39:50 -0300

    Finalizando teste baseado em especificação

--- leftpad/src/test/java/br/edu/utfpr/LeftPadTest.java
+++ leftpad/src/test/java/br/edu/utfpr/LeftPadTest.java
@@ -6,27 +6,53 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LeftPadTest {
 
     @Test
-    void testLeftPadWithPadding() {
+    void testStrParameter() {
+        // T01
+        assertNull(LeftPad.leftPad(null, 5, "-"));
+
+        // T02
+        assertEquals("-----", LeftPad.leftPad("", 5, "-"));
+
+        // T03
+        assertEquals("abc", LeftPad.leftPad("abc", 2, "-"));
+
+        // T04
         assertEquals("--abc", LeftPad.leftPad("abc", 5, "-"));
     }
 
     @Test
-    void testLeftPadWithoutPadding() {
-        assertEquals("abc", LeftPad.leftPad("abc", 3, "-"));
+    void testSizeParameter() {
+        // T08 (Negative size)
+        assertEquals("abc", LeftPad.leftPad("abc", -1, "-"));
+
+        // T08 (Large size)
+        assertEquals("-----------------abc", LeftPad.leftPad("abc", 20, "-"));
     }
 
     @Test
-    void testLeftPadWithNullString() {
-        assertNull(LeftPad.leftPad(null, 5, "-"));
+    void testPadStrParameter() {
+        // T05
+        assertEquals("  abc", LeftPad.leftPad("abc", 5, null));
+
+        // T06
+        assertEquals("  abc", LeftPad.leftPad("abc", 5, ""));
+
+        // T07
+        assertEquals("---abc", LeftPad.leftPad("abc", 6, "-"));
+
+        // T08
+        assertEquals("-=-abc", LeftPad.leftPad("abc", 6, "-="));
     }
 
     @Test
-    void testLeftPadWithEmptyPadString() {
-        assertEquals("   abc", LeftPad.leftPad("abc", 6, ""));
-    }
+    void testParameterCombinations() {
+        // T09
+        assertEquals("-=-=abc", LeftPad.leftPad("abc", 7, "-="));
 
-    @Test
-    void testLeftPadWithLargerSize() {
-        assertEquals("***abc", LeftPad.leftPad("abc", 6, "*"));
+        // T10
+        assertEquals("(O)(O)(O)(O)(O)(O)(O)(O)(O)abc", LeftPad.leftPad("abc", 30, "(O)"));
+
+        // T11
+        assertEquals("(O)(O)(O)(O)(O)(O)(O)(O)(abc", LeftPad.leftPad("abc", 28, "(O)"));
     }
 }
